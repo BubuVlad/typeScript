fea142c860151ca378b430521d4cedc798130bd6
Vlad <gheorghitavlad85@yahoo.com>
2021-04-21 11:29:17 +0300

    Remove + user.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { addFormProduct, getDataAndCreateInitialList } from "./scripts/controller";
+// import { createFormBasedOnProductProperties } from "./scripts/view";
 
-
+// createFormBasedOnProductProperties();
 getDataAndCreateInitialList();
 
 const addButton = document.getElementById('add-btn');
-
-addButton.addEventListener('click', addFormProduct)
+const formFields = document.querySelectorAll('.form-product input');
+addButton.addEventListener('click', function() {
+  addFormProduct(formFields)
+})
